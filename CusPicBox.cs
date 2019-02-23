dedf15ebea114a481e110bfc9f1d197bfee93500
airarabacal <47913040+airarabacal@users.noreply.github.com>
2019-02-23 15:48:23 +0800

    Add files via upload

---
+++ CusPicBox.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomizedControl
+{
+    public partial class CusPicBox : PictureBox
+    {
+        public event EventHandler ImageChanged;
+        public CusPicBox()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+        public virtual new Image Image
+        {
+            get
+            {
+                return base.Image;
+            }
+            set
+            {
+                base.Image = value;
+                if (this.ImageChanged != null)
+                    this.ImageChanged(this, new EventArgs());
+            }
+        }
+    }
+}
